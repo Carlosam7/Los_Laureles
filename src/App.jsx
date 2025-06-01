0edcee6d4d301ed5d830b4855451cd45846281b2
Carlos Arango <arangocarlos@uninorte.edu.co>
2025-06-01 16:09:20 -0500

    Definición de arquitectura del proyecto MVC

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,53 @@
 import { Button } from 'primereact/button';
-import { Calendar } from 'primereact/calendar';
+//import { Calendar } from 'primereact/calendar';
 
 import { useState } from 'react';
+import supabase from './utils/supabase';
 
 function App() {
-  const [date, setData] = useState(null);
-  const [value, setValue] = useState(1);
-  const options = [
-    { label: 'Option 1', value: 'Option 1' },
-    { label: 'Option 2', value: 'Option 2' },
-  ];
+  const [color, setColor] = useState('warning');
+  const listColors = ['warning', 'success', 'info', 'secondary', 'primary'];
+
+  const changeColor = () => {
+    setColor((prevColor) => {
+      const currentIndex = listColors.indexOf(prevColor);
+      const nextIndex = (currentIndex + 1);
+      if (nextIndex >= listColors.length) {
+        return listColors[0];
+      }
+      return listColors[nextIndex];
+    }
+  );
+  }
+
+  const response = async () => {
+    const { data } = (await supabase.from('room').select());
+
+    //Obtener los datos de la respuesta
+    if (data.error) {
+      console.error('Error fetching data:', data.error);
+      return [];
+    }
+
+    const rooms = data.map((room) => ({
+      id_room: room.id_room,
+      id_type: room.id_type,
+      capacity: room.capacity,
+      description: room.description,
+    }))
+
+    return rooms;
+  }
+  const data = response();
+
+  console.log(data);
+  
 
 
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Button label="Los Laureles" icon="pi pi-thumbs-up" className="p-button-rounded p-button-warning" />
+      <Button onClick={() => {changeColor()}} label="Los Laureles" icon="pi pi-thumbs-up" className={`p-button-rounded p-button-${color}`} />
     </div>
     </>
   )
