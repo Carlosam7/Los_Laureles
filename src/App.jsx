2776128638a2a173a030639fcf645e3999e89796
Esteban Dadul <edadul@uninorte.edu.co>
2025-06-04 15:53:55 -0500

    Profile page auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Login } from './pages/auth/Login';
 import { Page404 } from './pages/page404';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Profile } from './pages/client/profile';
 
 function App() {
   const navigate = useNavigate()
@@ -10,14 +11,22 @@ function App() {
   useEffect(() => {
     supabase.auth.onAuthStateChange((e, session) => {
       if (!session) { navigate('/login') }
-      else { navigate('/') }
+      else { navigate('/profile') }
     })
-  }, [])
 
+    const getSession = async () => {
+      const session = await supabase.auth.getSession()
+      console.log(session)
+    } 
+    getSession()
+
+  }, [])
+ 
   return (
     <>
       <Routes>
           <Route path='/login' element={<Login/>}/>
+          <Route path='/profile' element={<Profile/>}/>
           <Route path='*' element={<Page404/>}/>
       </Routes>
     </>
